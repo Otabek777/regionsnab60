0ec13ec9a509b97aa3c9f38b9cf05a43e44964d9
Otabek777 <53979293+Otabek777@users.noreply.github.com>
2022-02-24 05:03:52 +0500

    web page

--- js/main.js
+++ js/main.js
@@ -5,3 +5,50 @@ $(window).scroll(function(){
         $('.header').removeClass('active');
     };
 });
+
+document.querySelector('.header__catalog').addEventListener('click', function() {
+    this.classList.toggle("active");
+    document.querySelector('.black_bg').classList.toggle("active");
+    document.querySelector('.catalog_menu').classList.toggle('active');
+});
+document.querySelector('.black_bg').addEventListener('click', function() {
+    this.classList.remove('active');
+    document.querySelector('.header__catalog').classList.remove("active");
+    document.querySelector('.catalog_menu').classList.remove('active');
+});
+
+
+const accordeon = document.querySelectorAll('.catalog_menu__li');
+document.querySelector('body').addEventListener('click', function() {
+    for(let i = 0; i < accordeon.length; i++) {
+        accordeon[i].classList.remove('active');
+    }
+});
+
+accordeon.forEach(elements => {
+    elements.classList.remove('active');
+    elements.addEventListener('click', function() {
+        if(!elements.classList.contains('active')) {
+            accordeon.forEach (elements => {
+                elements.classList.remove('active');
+            });
+        };
+        setTimeout(function() {
+            elements.classList.toggle('active');
+        }, 01);
+    });
+});
+
+document.querySelector('.header__center .btn').addEventListener('click', function() {
+    document.querySelector('.modal').classList.add('active');
+    document.querySelector('body').classList.add('hidden');
+});
+
+const modalClose = document.querySelectorAll('.modal__close');
+for(let i = 0; i < modalClose.length; i++) {
+    modalClose[i].addEventListener('click', function() {
+        document.querySelector('.modal').classList.remove('active');
+        document.querySelector('.modal_done').classList.remove('active');
+        document.querySelector('body').classList.remove('hidden');
+    });
+};
