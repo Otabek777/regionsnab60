3f956751d78649ac923a9419fbdbc60d274daf42
Otabek777 <53979293+Otabek777@users.noreply.github.com>
2022-02-25 08:16:51 +0500

    mobile

--- js/main.js
+++ js/main.js
@@ -10,11 +10,14 @@ document.querySelector('.header__catalog').addEventListener('click', function() 
     this.classList.toggle("active");
     document.querySelector('.black_bg').classList.toggle("active");
     document.querySelector('.catalog_menu').classList.toggle('active');
+    document.querySelector('.header').classList.toggle('open');
+
 });
 document.querySelector('.black_bg').addEventListener('click', function() {
     this.classList.remove('active');
     document.querySelector('.header__catalog').classList.remove("active");
     document.querySelector('.catalog_menu').classList.remove('active');
+    document.querySelector('.header').classList.remove('open');
 });
 
 
@@ -50,5 +53,12 @@ for(let i = 0; i < modalClose.length; i++) {
         document.querySelector('.modal').classList.remove('active');
         document.querySelector('.modal_done').classList.remove('active');
         document.querySelector('body').classList.remove('hidden');
+    });
+};
+
+const footerH5 = document.querySelectorAll('.footer__box h5')
+for(let i = 0; i < footerH5.length; i++) {
+    footerH5[i].addEventListener('click', function() {
+        this.classList.toggle('active');
     });
 };
